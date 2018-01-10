d6ae82592a6038500c3215814a754f76ed7a25f0
github username <github email address>
2018-01-10 18:00:23 -0500

    basic controller

--- app/controllers/resume_entries_controller.rb
+++ app/controllers/resume_entries_controller.rb
@@ -1,2 +1,33 @@
 class ResumeEntriesController < ApplicationController
+
+  def new
+    @resume_entries = ResumeEntryn.new
+  end
+
+  def create
+    @resume_entry = Resume.new
+  end
+
+  def update
+    @resume_entry = ResumeEntry.find(params[:id])
+    if @resume_entry.update(resume_entry_params)
+      flash[:msg] = "Resume succesfully updated"
+      redirect_to @resume_entry
+    else
+      flash[:error] = @resume_entry.errors.errors.full_messages
+      redirect_to edit_resume_entry_path(@resume_entry)
+    end
+  end
+
+  def destroy
+    
+  end
+
+  private
+
+  def resume_entries_params
+    params.require(:resume_entry).permit(:resume_id, :entry_id)
+  end
+
+
 end
